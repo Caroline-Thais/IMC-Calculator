39bd4652adbb9c24f892cde0d0042e0ef0d08280
Thais <thaiscaroline.cms@gmail.com>
2022-06-11 09:10:17 -0300

    calculating imc

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import styles from './App.module.css'
 import { useState } from "react";
-import { levels, calculateImc } from './helpers/imc'
+import { levels, calculateImc, Level } from './helpers/imc'
 import { GridItem } from './components/GridItem';
+import leftArrowImage from './assets/leftarrow.png';
 
 const App = () => {
 
   const [heightField, setHeightField] = useState<number>(0);
   const [weightField, setWeightField] = useState<number>(0);
 
+  const [toShow, setToShow] = useState<Level | null>(null);
+
   const handleCalculateButton = () => {
     if (heightField && weightField) {
-
+      setToShow(calculateImc(heightField, weightField));
     } else {
       alert("Preencha todos os campos.")
     }
   }
 
+  const handleBackButton = () => {
+    setToShow(null);
+    setHeightField(0);
+    setWeightField(0);
+  }
   return (
     <div className={styles.main}>
       <header>
@@ -43,11 +51,21 @@ const App = () => {
           <button onClick={handleCalculateButton}>Calculate</button>
         </div>
         <div className={styles.rightSide}>
-          <div className={styles.grid}>
-            {levels.map((item, index) => (
-              <GridItem key={index} item={item} />
-            ))}
-          </div>
+          {!toShow &&
+            <div className={styles.grid}>
+              {levels.map((item, index) => (
+                <GridItem key={index} item={item} />
+              ))}
+            </div>
+          }
+          {toShow &&
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow} onClick={handleBackButton}>
+                <img src={leftArrowImage} alt="" width={25} />
+              </div>
+              <GridItem item={toShow} />
+            </div>
+          }
         </div>
       </div>
     </div>
